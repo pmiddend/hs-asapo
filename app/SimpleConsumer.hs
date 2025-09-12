68a8727c5437251ee2ad9dc9c0cb270bd002070b
Philipp Middendorf <philipp.middendorf@desy.de>
2025-09-12 10:03:00 +0200

    🐛 Fix and extend simple consumer

--- app/SimpleConsumer.hs
+++ app/SimpleConsumer.hs
@@ -1,21 +1,26 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
 import Asapo.Consumer
-import Control.Applicative (Applicative ((<*>)))
+import Control.Applicative (Applicative ((<*>)), pure)
+import Control.Exception (Exception, IOException, SomeException, catch)
 import Control.Monad (forM_, (=<<), (>>=))
 import Data.Bool (Bool (True))
 import Data.Either (Either (Left, Right))
+import Data.Foldable (for_)
 import Data.Function (($))
 import Data.Functor ((<$>))
-import Data.Maybe (Maybe (Nothing), fromMaybe)
+import Data.Int (Int)
+import Data.Maybe (Maybe (Just, Nothing), fromMaybe)
 import Data.Semigroup (Semigroup ((<>)))
 import Data.Text (Text, pack)
 import Data.Text.Encoding (decodeUtf8)
 import qualified Data.Text.IO as TIO
 import Data.Time.Clock (secondsToNominalDiffTime)
+import Data.Traversable (for)
 import qualified Options.Applicative as Opt
 import System.IO (IO)
 import Text.Show (Show (show))
@@ -26,7 +31,8 @@ hstoken = Token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjk1NzE3MTAyMTYsIm
 
 data Options = Options
   { optionsServerName :: Text,
-    optionsWithFilesystem :: Bool
+    optionsWithFilesystem :: Bool,
+    optionsMessageId :: Maybe Int
   }
 
 optionsParser :: Opt.Parser Options
@@ -34,6 +40,7 @@ optionsParser =
   Options
     <$> Opt.strOption (Opt.long "server-name")
     <*> Opt.switch (Opt.long "with-filesystem")
+    <*> Opt.option Opt.auto (Opt.long "message-id" <> Opt.value Nothing)
 
 main :: IO ()
 main = realMain =<< Opt.execParser opts
@@ -47,7 +54,7 @@ main = realMain =<< Opt.execParser opts
         )
 
 realMain :: Options -> IO ()
-realMain (Options serverName withFilesystem) = do
+realMain (Options serverName withFilesystem messageId) = do
   withConsumer
     (ServerName serverName)
     (SourcePath "")
@@ -68,7 +75,9 @@ realMain (Options serverName withFilesystem) = do
       TIO.putStrLn "misc: setting timeout"
       setTimeout consumer (secondsToNominalDiffTime 0.5)
 
-      getBeamtimeMeta consumer >>= \meta -> TIO.putStrLn $ "beamtime metadata: " <> (fromMaybe "N/A" meta)
+      TIO.putStrLn "getting beamtime metadata"
+      beamtimeMeta <- getBeamtimeMeta consumer `catch` (\(e :: SomeException) -> pure Nothing)
+      TIO.putStrLn $ "beamtime metadata: " <> fromMaybe "N/A" beamtimeMeta
 
       TIO.putStrLn "listing all available streams:"
       streams <- getStreamList consumer Nothing FilterAllStreams
@@ -76,18 +85,26 @@ realMain (Options serverName withFilesystem) = do
         TIO.putStrLn $ "=> stream info " <> pack (show stream)
         streamSize <- getCurrentSize consumer (streamInfoName stream)
         TIO.putStrLn $ "   stream size: " <> pack (show streamSize)
-        datasetCount <- getCurrentDatasetCount consumer (streamInfoName stream) IncludeIncomplete
+        datasetCount <- getCurrentDatasetCount consumer (streamInfoName stream) IncludeIncomplete `catch` (\(e :: SomeException) -> pure 0)
         TIO.putStrLn $ "   dataset count: " <> pack (show datasetCount)
 
-      -- withGroupId consumer outputError \groupId -> do
-      --   onSuccess "getNextMessageMeta" (getNextMessageMeta consumer (streamInfoName stream) groupId) \(messageMetaHandle, messageMeta) -> do
-      --     TIO.putStrLn "   got message meta"
-      --   onSuccess "getNextMessageMetaAndData" (getNextMessageMetaAndData consumer (streamInfoName stream) groupId) \(messageMetaHandle, messageMeta, messageData) -> do
-      --     TIO.putStrLn "   got message"
+        -- withGroupId consumer outputError \groupId -> do
+        --   onSuccess "getNextMessageMeta" (getNextMessageMeta consumer (streamInfoName stream) groupId) \(messageMetaHandle, messageMeta) -> do
+        --     TIO.putStrLn "   got message meta"
+        --   onSuccess "getNextMessageMetaAndData" (getNextMessageMetaAndData consumer (streamInfoName stream) groupId) \(messageMetaHandle, messageMeta, messageData) -> do
+        --     TIO.putStrLn "   got message"
 
-      (meta, data') <- getMessageMetaAndDataById consumer (StreamName "default") (messageIdFromInt 156)
-      TIO.putStrLn $ "meta: " <> pack (show meta)
-      TIO.putStrLn $ "data: " <> decodeUtf8 data'
+        case messageId of
+          Just mid -> do
+            (meta, data') <- getMessageMetaAndDataById consumer (streamInfoName stream) (messageIdFromInt mid)
+            TIO.putStrLn $ "    meta: " <> pack (show meta)
+            TIO.putStrLn $ "    data: " <> decodeUtf8 data'
+          Nothing -> do
+            TIO.putStrLn "    messages from stream:"
+            withGroupId consumer \groupId -> do
+              (meta, data') <- getNextMessageMetaAndData consumer (streamInfoName stream) groupId
+              TIO.putStrLn $ "      meta: " <> pack (show meta)
+              TIO.putStrLn $ "      data: " <> decodeUtf8 data'
 
       TIO.putStrLn "misc: resending nacs"
       resendNacs consumer True (secondsToNominalDiffTime 1) 10
